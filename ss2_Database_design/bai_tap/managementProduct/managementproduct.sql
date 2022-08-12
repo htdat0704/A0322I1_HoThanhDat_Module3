94aba99811fcae9ca02aede3a9b346cedad6e213
htdat0704 <htdat.18it3@sict.udn.vn>
2022-08-12 18:55:54 +0700

    first commit

---
+++ ss2_Database_design/bai_tap/managementProduct/managementproduct.sql
@@ -0,0 +1,34 @@
+create database QuanLyBanHang ;
+
+use QuanLyBanHang ;
+
+create table Customer(
+	cID int,
+    cName varchar(50),
+    cAge int,
+    primary key(cID)
+);
+
+create table `Order` (
+	oID int,
+    cID int,
+    oDate date,
+    oTotalPrice float,
+    primary key(oID),
+    foreign key (cID) references Customer(cID)
+);
+
+create table Product (
+	pID int,
+    pName varchar(50),
+    pPrice float,
+    primary key (pID)
+);
+
+create table OrderDetail (
+	oID int,
+    pID int,
+    odQTY int,
+    foreign key (oID) references `Order`(oID),
+    foreign key (pID) references Product(pID)
+);
